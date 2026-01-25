d17c14d0b2d460e3672c2210eaf9f8c49a4285a8
manouvanderheijden <manouzhg@gmail.com>
2026-01-25 16:40:44 +0100

    grafiekjes van de functies

---
+++ plot_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy.special as sp
+import math
+import matplotlib.pyplot as plt
+
+from supplementary import Value
+
+from activation_functions import identity, relu, logi, step, softmax, tanh, sin, silu, softsign, softplus, erf
+
+
+functions = [identity, relu, logi, step, softmax, tanh, sin, silu, softsign, softplus, erf]
+x_axis = Value(np.linspace(-5,5, 1000), "x-as")
+
+for func in functions:
+    plt.title(f"{func.__name__}")
+    plt.plot(x_axis.data, func(x_axis).data)
+    plt.grid()
+    plt.show()
