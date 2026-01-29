a4bdd4e509cea94b5e667f08e52893d6b35efaff
manouvanderheijden <manouzhg@gmail.com>
2026-01-29 12:19:27 +0100

    grafiekjes van de functies met grotere letters

--- plot_functions.py
+++ plot_functions.py
@@ -10,8 +10,14 @@ from activation_functions import identity, relu, logi, step, softmax, tanh, sin,
 
 functions = [identity, relu, logi, step, softmax, tanh, sin, silu, softsign, softplus, erf]
 x_axis = Value(np.linspace(-5,5, 1000), "x-as")
+font = {#'family' : 'normal',
+        #'weight' : 'bold',
+        'size'   : 22}
+
 
 for func in functions:
+    plt.figure()
+    plt.rc('font', **font)
     plt.title(f"{func.__name__}")
     plt.plot(x_axis.data, func(x_axis).data)
     plt.grid()
